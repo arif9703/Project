3aced4cb42c7723b6f0eb75a0a8e8aa95e21011c
arif hosen <arifhosen9766@gmail.com>
2022-10-14 02:23:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// ES6 Syntax
+let x=33;
+for(let x= 1; x<=10; x++){
+    console.log(x)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Arrow Funtion
+// Destruction
+// String Methods
+// Array Methods
+// ES6 Modules
+// ES6 Classes
+// Promise, Async
+// 
+// 
+// 
+// 
+// 
